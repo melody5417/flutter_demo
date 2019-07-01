e00dcec3f94d1d6e9e23c7c46ffeb6f901fa46b4
yiqiwang(王一棋) <yiqiwang@tencent.com>
2019-07-01 16:49:26 +0800

    add demo for top bar

---
+++ flutter_demo/lib/Tab/top_tab.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+
+class WYQTopBar extends StatefulWidget {
+  @override
+  _WYQTopBarState createState() => _WYQTopBarState();
+}
+
+class _WYQTopBarState extends State<WYQTopBar> with SingleTickerProviderStateMixin {
+  final List<Tab> myTabs = <Tab>[
+    Tab(text: 'LEFT'),
+    Tab(text: 'RIGHT'),
+  ];
+
+  TabController _tabController;
+
+  @override
+  void initState() {
+    super.initState();
+    _tabController = TabController(length: myTabs.length, vsync: this);
+  }
+
+  @override
+  void dispose() {
+    _tabController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        title: Text("顶部tab"),
+        bottom: TabBar(
+          controller: _tabController,
+          tabs: myTabs,
+        ),
+      ),
+      body: TabBarView(
+        controller: _tabController,
+        children: myTabs.map((Tab tab) {
+          return Center(child: Text(tab.text));
+        }).toList(),
+      ),
+    );
+  }
+}
